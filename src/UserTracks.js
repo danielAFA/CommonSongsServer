8b53f73683a22978971eb6a250c36157fbac38a9
Daniel Fuentes <fuentes.danav@gmail.com>
2020-09-06 00:04:50 -0500

    UserTrack class for managing track data and generating track list intersections, added routes for accessing intersection

---
+++ src/UserTracks.js
@@ -0,0 +1,56 @@
+class UserTracks {
+  constructor() {
+    this.userData = []
+    this.maxUsers = 2
+    this.currentUsers = 0
+    this.intersection = null
+  }
+
+  getCurrentUsers() {
+    return this.currentUsers
+  }
+
+  getUserData() {
+    let users = {}
+    for (const elem of this.userData) {
+      users = { ...users, ...elem }
+    }
+    return users
+  }
+
+  getIntersection() {
+    if (this.currentUsers !== this.maxUsers) return null
+    return this.intersection
+  }
+
+  createIntersection() {
+    if (this.currentUsers < this.maxUsers) {
+      return
+    }
+
+    const [user1Tracks, user2Tracks] = this.userData.map(
+      (el, i) => this.userData[i][Object.keys(el)[0]]
+    )
+
+    const instersection = user1Tracks.filter(user1Track =>
+      user2Tracks.some(y => user1Track.track.id === user2Track.track.id)
+    )
+
+    this.intersection = instersection
+  }
+
+  setUser(user) {
+    if (!user) {
+      console.log('invalid user entered')
+      return
+    }
+
+    this.userData.push(user)
+    if (this.currentUsers >= this.maxUsers) {
+      this.userData.shift()
+    } else if (this.currentUsers < this.maxUsers) this.currentUsers++
+    this.createIntersection()
+  }
+}
+
+module.exports = UserTracks
